fb42ae59bf5a024b6fd8f063905b2984a6c72422
as3eem <as3eem@gmail.com>
2020-05-16 11:31:06 +0530

    mongo atlas configuration

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-    database:'mongodb://localhost:27017/nodekb',
+    // database:'mongodb://localhost:27017/nodekb',
+    database: 'mongodb+srv://as3eem:qwertyatlas@imaginehuge-db-mb6x7.gcp.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'yoursecret'
   }
   
